47d0532a0ff3547ec07d8de2dd0d7bbee85e57d6
sisu-01 <tesv010615@naver.com>
2025-11-17 16:08:01 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
   
   return (
     <div className="flex flex-col h-screen">
-      <BrowserRouter>
+      <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}>
         <Header />
         <main className="flex-1 relative w-full h-full">
           <Routes>
