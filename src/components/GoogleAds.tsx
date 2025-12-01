b3b97b8da21a8af1e9f5b207f857075d4c1dfe83
sisu-01 <tesv010615@naver.com>
2025-12-01 13:42:32 +0900

    광고?

--- src/components/GoogleAds.tsx
+++ src/components/GoogleAds.tsx
@@ -1,7 +1,7 @@
 const GoogldAds = () => {
   // w-[728px]
   return (
-    <div className="bg-white w-full h-[90px] flex justify-center items-center text-2xl font-bold border-1">
+    <div className="bg-white w-full h-full flex justify-center items-center text-2xl font-bold border-1">
       구글 애드센스
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import GoogldAds from "./GoogleAds";
 const Header = () => {
   return (
     <div className="bg-black">
-      <div className="container mx-auto px-4 flex justify-center items-center gap-2">
+      <div className="container mx-auto h-[60px] sm:h-[100px] px-4 flex justify-center items-center gap-2">
         <GoogldAds />
         <GoogldAds />
       </div>
