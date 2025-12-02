2c301aaa39e9c57fde93fc78032e116d45101107
sisu-01 <tesv010615@naver.com>
2025-12-02 14:55:14 +0900

    헤더도 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className="bg-black">
       <div className="container mx-auto h-[60px] sm:h-[100px] px-4 flex justify-center items-center gap-2">
-        <GoogldAds />
-        <GoogldAds />
+        {/* <GoogldAds /> */}
+        {/* <GoogldAds /> */}
       </div>
     </div>
   );
