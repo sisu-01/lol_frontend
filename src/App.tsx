2909c6c72bdb958e23567a909e26b70519fdbfa7
sisu-01 <tesv010615@naver.com>
2026-01-05 16:57:46 +0900

    상단하단숨겨두기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import GamePage from "./pages/game/GamePage";
-import AdBar from "./components/AdBar";
+// import AdBar from "./components/AdBar";
 import GATracker from "./components/GATracker";
 
 const App = () => {
@@ -10,14 +10,15 @@ const App = () => {
     <div className="flex flex-col h-dvh">
       <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}>
         <GATracker />
-        <AdBar position='header' />
-        <main className="absolute top-[60px] bottom-[60px] sm:top-[90px] sm:bottom-[90px] overflow-hidden flex-1 w-full">
+        {/* <AdBar position='header' /> */}
+        {/* <main className="absolute top-[60px] bottom-[60px] sm:top-[90px] sm:bottom-[90px] overflow-hidden flex-1 w-full"> */}
+        <main className="overflow-hidden flex-1 w-full">
           <Routes>
               <Route path='/' element={<HomePage />} />
               <Route path='/game' element={<GamePage />} />
           </Routes>
         </main>
-        <AdBar position='footer' />
+        {/* <AdBar position='footer' /> */}
       </BrowserRouter>
     </div>
   )
